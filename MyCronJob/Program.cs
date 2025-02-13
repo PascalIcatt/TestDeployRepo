712007f12658cc1e41e64e7e7200f27cdb1e0840
Pascal van Oostenbrugge <pascal@icatt.nl>
2025-02-13 13:03:55 +0100

    Initial commit

---
+++ MyCronJob/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine($"CronJob executed at {DateTime.UtcNow}");
+        await Task.Delay(1000);
+    }
+}
